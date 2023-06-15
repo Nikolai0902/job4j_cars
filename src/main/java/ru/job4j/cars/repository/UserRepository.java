5d59ba883f1c85fc22d17bb1880c2a272ca91849
Nikolai0902 <kbus94@yandex.ru>
2023-06-15 23:11:11 +0300

    Добавлен UserRepository для работы с обьектом User, UserUsage для экспериментов.

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,166 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import ru.job4j.cars.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Реализация методов работы с обьектом User,
+ * через язык Hibernate Query Language (далее - HQL).
+ */
+@AllArgsConstructor
+public class UserRepository {
+
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE User SET login = :fLogin, password = :fPassword WHERE id = :fId")
+                    .setParameter("fLogin", user.getLogin())
+                    .setParameter("fPassword", user.getPassword())
+                    .setParameter("fId", user.getId())
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE User WHERE id = :id")
+                    .setParameter("id", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        List<User> users = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User order by id ASC", User.class);
+            users = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return users;
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int userId) {
+        Session session = sf.openSession();
+        Optional<User> user = Optional.empty();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User WHERE id = :id", User.class);
+            query.setParameter("id", userId);
+            user = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return user;
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        List<User> users = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User WHERE login LIKE :key", User.class);
+            query.setParameter("key", "%" + key + "%");
+            users = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return users;
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        Optional<User> user = Optional.empty();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User WHERE login = :login", User.class);
+            query.setParameter("login", login);
+            user = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return user;
+    }
+}
---
+++ src/main/java/ru/job4j/cars/repository/UserUsage.java
@@ -0,0 +1,49 @@
+package ru.job4j.cars.repository;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import ru.job4j.cars.model.User;
+
+/**
+ * Проверка методов на языке HQL класса UserReposition -
+ * вставка, вывод, изменение, удаление обьекта User.
+ *
+ * SessionFactory - это объект конфигуратор. Он создается один раз на все приложение.
+ * В нем происходит создания пулов, загрузка кешей, проверка моделей.
+ * SessionFactory имеет метод openSession, который позволяет записать,
+ * удалить и прочитать данные из базы. Этот объект создается быстро.
+ */
+public class UserUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (SessionFactory sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            var userRepository = new UserRepository(sf);
+            var user = new User();
+            user.setLogin("admin");
+            user.setPassword("admin");
+            userRepository.create(user);
+            userRepository.findAllOrderById()
+                    .forEach(System.out::println);
+            userRepository.findByLikeLogin("e")
+                    .forEach(System.out::println);
+            userRepository.findById(user.getId())
+                    .ifPresent(System.out::println);
+            userRepository.findByLogin("admin")
+                    .ifPresent(System.out::println);
+            user.setPassword("password");
+            userRepository.update(user);
+            userRepository.findById(user.getId())
+                    .ifPresent(System.out::println);
+            userRepository.delete(user.getId());
+            userRepository.findAllOrderById()
+                    .forEach(System.out::println);
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
