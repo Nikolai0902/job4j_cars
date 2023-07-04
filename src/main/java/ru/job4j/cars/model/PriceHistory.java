558dffe6887d452a93336e2c009f84f48e8e367d
Nikolai0902 <kbus94@yandex.ru>
2023-07-04 22:42:06 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,21 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "price_history")
+public class PriceHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int before;
+
+    private int after;
+
+    private LocalDateTime created = LocalDateTime.now();
+}
