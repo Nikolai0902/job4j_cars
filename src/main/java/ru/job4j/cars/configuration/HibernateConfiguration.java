8907a9f427a61b0441a77601dfa9f6818dc94c86
Nikolai0902 <kbus94@yandex.ru>
2023-09-14 01:14:45 +0300

    Добавлены представления html

--- src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
+++ src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
@@ -7,9 +7,20 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Класс конфигурирования/
+ * @author Buslaev
+ */
 @Configuration
 public class HibernateConfiguration {
 
+    /**
+     * SessionFactory - это объект конфигуратор. Он создается один раз на все приложение.
+     * В нем происходит создания пулов, загрузка кешей, проверка моделей.
+     * SessionFactory имеет метод openSession, который позволяет записать,
+     * удалить и прочитать данные из базы. Этот объект создается быстро.
+     * @return SessionFactory
+     */
     @Bean(destroyMethod = "close")
     public SessionFactory sf() {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
